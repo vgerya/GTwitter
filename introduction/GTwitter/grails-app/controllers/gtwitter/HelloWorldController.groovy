c79e31c225685fe7558bd6088b8d7ee295b9f7e5
Vitaliy Gerya <Vitaliy.Gerya@gmail.com>
2011-09-08 15:54:58 +0300

    added dependencies to domains

--- introduction/GTwitter/grails-app/controllers/gtwitter/HelloWorldController.groovy
+++ introduction/GTwitter/grails-app/controllers/gtwitter/HelloWorldController.groovy
@@ -2,11 +2,5 @@ package gtwitter
 
 class HelloWorldController {
 
-	def index = {  [name: params.name] }
-
-	def show = {  }
-	
-	static def mapping ={
-		
-	}
+	def index = {		 [name: params.name] }
 }
--- introduction/GTwitter/grails-app/domain/gtwitter/User.groovy
+++ introduction/GTwitter/grails-app/domain/gtwitter/User.groovy
@@ -11,14 +11,14 @@ class User {
 	boolean accountLocked
 	boolean passwordExpired
 
+	static hasMany = [messages: Message]
+
 	static constraints = {
 		username blank: false, unique: true
 		password blank: false
 	}
 
-	static mapping = {
-		password column: '`password`'
-	}
+	static mapping = { password column: '`password`' }
 
 	Set<Role> getAuthorities() {
 		UserRole.findAllByUser(this).collect { it.role } as Set
